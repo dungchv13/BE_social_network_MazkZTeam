f519c747c52741a5d0a5c3bb3ecc1e9dab2c05b1
unknown <dungutc13@gmail.com>
2021-02-01 17:00:08 +0700

    structure project

---
+++ src/main/java/socialnetwork/mazkzteam/MazkzteamApplication.java
@@ -0,0 +1,13 @@
+package socialnetwork.mazkzteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazkzteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazkzteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/socialnetwork/mazkzteam/MazkzteamApplicationTests.java
@@ -0,0 +1,13 @@
+package socialnetwork.mazkzteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazkzteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
