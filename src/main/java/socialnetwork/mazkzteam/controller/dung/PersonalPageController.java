f093745a3dadbc0caf4033210f6e74482f4240fd
conghieu436789 <nguyenconghieu17494@gmail.com>
2021-02-08 01:15:14 +0700

    edit API personalPage

--- src/main/java/socialnetwork/mazkzteam/controller/dung/PersonalPageController.java
+++ src/main/java/socialnetwork/mazkzteam/controller/dung/PersonalPageController.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/{username}")
+@RequestMapping("/personal/{username}")
 @CrossOrigin("*")
 public class PersonalPageController {
 
