7e10b6a73d139da72e26890d07a6b2d62134ccc2
unknown <dungutc13@gmail.com>
2021-02-19 15:01:07 +0700

    them clup api

--- src/main/java/socialnetwork/mazkzteam/model/repositories/ClubRepository.java
+++ src/main/java/socialnetwork/mazkzteam/model/repositories/ClubRepository.java
@@ -1,7 +1,18 @@
 package socialnetwork.mazkzteam.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import socialnetwork.mazkzteam.model.entities.Club;
+import socialnetwork.mazkzteam.model.entities.User;
+
+import java.util.List;
 
 public interface ClubRepository extends JpaRepository<Club,Integer> {
+    @Query(value = "SELECT *\n" +
+            "FROM club where founder_id = ?1",nativeQuery = true)
+    List<Club> clubListByUserCreate(Integer id);
+
+    List<Club> getClubByMembersContains(User user);
+
+    List<Club> getClubsByMembersIsNotContaining(User user);
 }
--- src/main/java/socialnetwork/mazkzteam/model/service/ClubService.java
+++ src/main/java/socialnetwork/mazkzteam/model/service/ClubService.java
@@ -1,6 +1,14 @@
 package socialnetwork.mazkzteam.model.service;
 
 import socialnetwork.mazkzteam.model.entities.Club;
+import socialnetwork.mazkzteam.model.entities.User;
+
+import java.util.List;
 
 public interface ClubService extends CommonService<Club> {
+    List<Club> getClubsByUserCreate(int user_id);
+
+    List<Club> getClubByMembersContains(User user);
+
+    List<Club> getClubsByMembersIsNotContaining(User user);
 }
