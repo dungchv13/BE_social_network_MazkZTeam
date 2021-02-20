0d3b009004d038045a9d73f23d8cd1e9bf5dffe5
conghieu436789 <nguyenconghieu17494@gmail.com>
2021-02-20 10:36:36 +0700

    fix queery postcommonfriend

--- src/main/java/socialnetwork/mazkzteam/model/repositories/PostRepository.java
+++ src/main/java/socialnetwork/mazkzteam/model/repositories/PostRepository.java
@@ -14,7 +14,7 @@ public interface PostRepository extends JpaRepository<Post,Integer> {
             "inner join (SELECT * FROM social_network.friendship\n" +
             "where user_receiver_id=?1 or user_sender_id=?1) as f\n" +
             "on post.user_id = f.user_receiver_id or post.user_id = f.user_sender_id\n" +
-            "where user_id =?1 or user_id<>?1 and (protective=1 or protective=2)\n" +
+            "where (user_id =?1 or user_id<>?1 and (protective=1 or protective=2)) and club_id=9999\n" +
             "group by created_date order by created_date desc;", nativeQuery = true)
     List<Post> findAllCommonFriendPublicPost(int id);
 }
